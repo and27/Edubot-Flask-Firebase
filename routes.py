c44c19968b8234488d6a5de6fca0aa42aa77c149
Andrés Banda <andrezh_gln7@live.com>
2020-09-04 01:26:51 +0000

    Server changes

--- routes.py
+++ routes.py
@@ -36,4 +36,4 @@ def juegos():
     return render_template('search.html', the_title='Búsqueda de Juegos')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5050", debug=True)
