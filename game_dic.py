fff96b3092e3d07788644037b619cae134c73f50
andro <andrezh_gln7@live.com>
2020-09-06 23:37:29 -0500

    Added base template for games

---
+++ game_dic.py
@@ -0,0 +1,19 @@
+game_dic = [
+	{"game_title":"Juego 5 Enteros",
+	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec nisl tincidunt, pulvinar est at, sollicitudin orci. Morbi vel accumsan elit. Aliquam tempus ligula eget nisl varius, sed dictum elit vulputate. Sed a convallis leo, in dictum metus. Ut elementum bibendum neque sed venenatis.",
+	"game_competence":"In eu felis porta, convallis neque id, rutrum nunc. Curabitur non convallis est, ac egestas lorem. Cras aliquam suscipit mauris vel gravida. In tempus erat lectus, vel mattis sem pellentesque a. Vivamus augue risus, luctus at tempor a, cursus quis lorem.",
+	"game_level":4,
+	"game_image":"banner_space.png"
+	},
+	{"game_title":"Juego Líneas Rectas",
+	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec nisl tincidunt, pulvinar est at, sollicitudin orci. Morbi vel accumsan elit. Aliquam tempus ligula eget nisl varius, sed dictum elit vulputate. Sed a convallis leo, in dictum metus. Ut elementum bibendum neque sed venenatis.",
+	"game_competence":"In eu felis porta, convallis neque id, rutrum nunc. Curabitur non convallis est, ac egestas lorem. Cras aliquam suscipit mauris vel gravida. In tempus erat lectus, vel mattis sem pellentesque a. Vivamus augue risus, luctus at tempor a, cursus quis lorem.",
+	"game_level":4,
+	"game_image":"banner_space.png"
+	}
+
+
+
+
+
+]
