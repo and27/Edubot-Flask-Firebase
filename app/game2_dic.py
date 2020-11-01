9a5539438a9c27079cf26788f142fb496f802628
shirlizz <shirleylizz.98@gmail.com>
2020-11-01 18:17:36 -0500

    Modified dynamic ranking

--- app/game2_dic.py
+++ app/game2_dic.py
@@ -3,20 +3,22 @@ game2_dic = [
 	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	"game_dificulty":"Facil",
 	"game_image":"game_black.png",
-	"game_category":"Matematicas"
+	"game_category":"Matematicas",
+	"rank":4 
 	},
 	{"game_title":"Juego Líneas Rectas",
 	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	"game_dificulty":"Intermedio",
 	"game_image":"game_black.png",
-	"game_category":"Matematicas"
-
+	"game_category":"Matematicas",
+	"rank":2
 	},
 
 	{"game_title":"Juego Multiplicaciones",
 	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	"game_dificulty":"Dificil",
 	"game_image":"game_black.png",
-	"game_category":"Matematicas"
+	"game_category":"Matematicas",
+	"rank":1
 	}
 ]
