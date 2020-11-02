5d330d7e3c885b52e51422e8b62371495c3d1b90
edmar2407 <edwinvasconez1996@gmail.com>
2020-11-01 23:12:41 -0500

    Added more games

--- app/game2_dic.py
+++ app/game2_dic.py
@@ -20,5 +20,49 @@ game2_dic = [
 	"game_image":"game_black.png",
 	"game_category":"Matematicas",
 	"rank":1
+	},
+	{"game_title":"Juego 5 Enteros",
+	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	"game_dificulty":"Facil",
+	"game_image":"game_black.png",
+	"game_category":"Matematicas",
+	"rank":4 
+	},
+	{"game_title":"Juego Líneas Rectas",
+	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	"game_dificulty":"Intermedio",
+	"game_image":"game_black.png",
+	"game_category":"Matematicas",
+	"rank":2
+	},
+
+	{"game_title":"Juego Multiplicaciones",
+	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	"game_dificulty":"Dificil",
+	"game_image":"game_black.png",
+	"game_category":"Matematicas",
+	"rank":1
+	},
+	{"game_title":"Juego 5 Enteros",
+	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	"game_dificulty":"Facil",
+	"game_image":"game_black.png",
+	"game_category":"Matematicas",
+	"rank":4 
+	},
+	{"game_title":"Juego Líneas Rectas",
+	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	"game_dificulty":"Intermedio",
+	"game_image":"game_black.png",
+	"game_category":"Matematicas",
+	"rank":2
+	},
+
+	{"game_title":"Juego Multiplicaciones",
+	"game_description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	"game_dificulty":"Dificil",
+	"game_image":"game_black.png",
+	"game_category":"Matematicas",
+	"rank":1
 	}
 ]
